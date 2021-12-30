0e83e06f8d10f7773d6588d8f3578ea97504d963
Samantha Malone <sammac99@gmail.com>
2021-12-29 17:22:55 -0700

    added body-parser

--- project/server/app.js
+++ project/server/app.js
@@ -1,10 +1,13 @@
 //Import all dependencies and Middleware
 import express from "express";
+import bodyParser from "body-parser";
 
 //Init Express App
 const app = express();
 
 //Dependencies used
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Controllers (APIs) used
 app.get("/", (req, res) => {
