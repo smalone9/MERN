4377ec38fe725c5cee88da513055ad51db1f8feb
Samantha Malone <sammac99@gmail.com>
2022-01-08 13:46:13 -0700

    connected router and App.js

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RouterComponent from './router/RouterComponent';
 
 import Landing from './components/Landing';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Landing />
+        <RouterComponent />
       </div>
     );
   }
