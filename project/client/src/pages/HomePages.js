3854a382d5e9fb06569f7c8a5f589b0e29c91c0c
Samantha Malone <sammac99@gmail.com>
2022-01-08 13:17:39 -0700

    added HomePage as React Class

---
+++ project/client/src/pages/HomePages.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Landing from '../components/Landing';
+
+class HomePage extends Component {
+  render() {
+    return <Landing />;
+  }
+}
+
+export default HomePage;
