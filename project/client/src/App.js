a6702352f53e2a03836646502070b64badb845e0
Samantha Malone <sammac99@gmail.com>
2022-01-08 13:54:31 -0700

    combined Router, Redux-thunk, and App

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import RouterComponent from './router/RouterComponent';
 
-import Landing from './components/Landing';
+import { setUserToken } from './store/actions/auth.action';
+import { USER_TOKEN } from './constants';
 
 class App extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem(USER_TOKEN);
+    if (token) {
+      this.props.setUserToken({ token });
+    }
+  }
+
   render() {
+    const { user } = this.props || {};
     return (
       <div>
-        <RouterComponent />
+        <RouterComponent user={user} />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ setUserToken }, dispatch);
+}
+export default connect(mapStateToProps, matchDispatchToProps)(App);
