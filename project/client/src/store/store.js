1a67b2847c65aa000c933479bbfa2273b0b613a6
Samantha Malone <sammac99@gmail.com>
2022-01-08 14:17:22 -0700

    debugged store.js

--- project/client/src/store/store.js
+++ project/client/src/store/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-think';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhance = [applyMiddleware(thunk)];
+const enhancer = [applyMiddleware(thunk)];
 
 const store = createStore(
   rootReducer,
